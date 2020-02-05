2c479747599977348c7b0c4abb90a643264bdcca
kusanath <47378688+kusanath@users.noreply.github.com>
2020-02-05 11:45:02 +0530

    Add files via upload

---
+++ command.sh
@@ -0,0 +1,43 @@
+#shell script to show system configrations
+# program by KUSANATH R
+echo
+echo PROGRAM TO SHOW SYSTEM CONFIGRATIONS
+echo
+echo current logged user and logged name
+echo $LOGNAME
+echo
+
+echo current shell
+echo $SHELL
+echo
+
+
+echo current directory
+echo $HOME
+echo
+
+
+echo operating system Type
+echo $OSTYPE
+echo
+
+
+echo system architecture
+echo $(arch)
+echo
+
+
+echo path setting
+echo $PATH
+echo
+
+
+echo current working directory 
+echo $PWD
+echo
+
+
+echo NUMBER OF LOGGED IN USERS
+echo $USER
+echo
+
