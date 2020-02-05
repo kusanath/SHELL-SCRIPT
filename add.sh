e0101edbe91855d5728ce67a5f83e794faaa39cc
kusanath <47378688+kusanath@users.noreply.github.com>
2020-02-05 10:55:26 +0530

    Add files via upload

---
+++ add.sh
@@ -0,0 +1,7 @@
+echo enter a number1
+read number1
+echo enter a number2
+read number2
+#sum=$(($number1+$number2))
+echo the value  is $(($number1+$number2))
+
---
+++ prog.sh
@@ -0,0 +1,4 @@
+echo enter a number1
+read number1
+
+echo value of the variable is $number
